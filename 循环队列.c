80d8ae7485f83936f25808301b6feb7dc58019d9
TheZHENXI <111845931+TheZHENXI@users.noreply.github.com>
2022-10-15 00:26:29 +0800

    Update 循环队列.c

--- 循环队列.c
+++ 循环队列.c
@@ -7,6 +7,8 @@ typedef struct
     int k;
 } MyCircularQueue;
 
+bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+bool myCircularQueueIsFull(MyCircularQueue* obj) ;
 
 MyCircularQueue* myCircularQueueCreate(int k) 
 {
@@ -21,35 +23,42 @@ MyCircularQueue* myCircularQueueCreate(int k)
 
 bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) 
 {
-    if(myCircularQueueIsFull) 
+    if(myCircularQueueIsFull(obj)) 
         return false;
 
     obj->data[obj->tail] = value;
     obj->tail++;
-    obj->tail %= k+1;//取模防止越界访问
-    return ture;
+    obj->tail %= obj->k + 1;//取模防止越界访问
+    return true;
 }
 
 bool myCircularQueueDeQueue(MyCircularQueue* obj) 
 {
-    if(myCircularQueueIsEmpty) 
+    if(myCircularQueueIsEmpty(obj)) 
         return false;
     
     obj->front++;
-    obj->front %= k+1;//取模防止越界访问
+    obj->front %= obj->k + 1;//取模防止越界访问
     return true;
 }
 
 int myCircularQueueFront(MyCircularQueue* obj) 
 {
+    if(myCircularQueueIsEmpty(obj)) //调用时总是忘写实参，导致一直在此终止程序
+        return -1;
+
     return obj->data[obj->front];
 }
 
-int myCircularQueueRear(MyCircularQueue* obj) {
-    if(obj->tail == 0)
-        return obj->data[k];
+int myCircularQueueRear(MyCircularQueue* obj) 
+{
+    if(myCircularQueueIsEmpty(obj)) //调用时总是忘写实参，导致一直在此终止程序
+        return -1;
+
+    if(obj->tail == 0)//防止tail在0位置的时候
+        return obj->data[obj->k];
     else
-        return obj->data[obj->tail];
+        return obj->data[obj->tail - 1];
 }
 
 bool myCircularQueueIsEmpty(MyCircularQueue* obj) 
@@ -59,7 +68,7 @@ bool myCircularQueueIsEmpty(MyCircularQueue* obj)
 
 bool myCircularQueueIsFull(MyCircularQueue* obj) 
 {
-    if((obj->tail+1)%(k+1) == obj->front)
+    if((obj->tail + 1)%(obj->k + 1) == obj->front)
         return true;
     else
         return false;
@@ -67,7 +76,10 @@ bool myCircularQueueIsFull(MyCircularQueue* obj)
 
 void myCircularQueueFree(MyCircularQueue* obj) 
 {
-
+    free(obj->data);
+    obj->data = NULL;
+    free(obj);
+    obj = NULL;
 }
 
 /**
