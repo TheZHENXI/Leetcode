16a81e62e41a643d42eb920e101f6421c93a6baa
TheZHENXI <111845931+TheZHENXI@users.noreply.github.com>
2022-10-15 16:24:50 +0800

    空栈（负责myStackPop时存入前n-1个元素），非空栈（负责myStackPush）

--- 两个队列实现栈.c
+++ 两个队列实现栈.c
@@ -1,292 +1,195 @@
+
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 #include <stdlib.h>
+#include <assert.h>
 
-
-// #include "contact.h"
-
-// *********************************************
-#define MAX 1000
-#define MAX_NAME 20
-#define MAX_SEX 5
-#define MAX_TEL 12
-#define MAX_AD 20
-#define CAPACITY 3
-//个人信息
-struct PeoInfo
+typedef int QueueDataType;
+typedef struct QueueNode
 {
-    char name[MAX_NAME];
-    char Sex[MAX_SEX];
-    char Tele[MAX_TEL];
-    char AD[MAX_AD];
-    /* data */
-};
-//通讯录类型
-struct Contact 
+    struct QueueNode* next;
+    QueueDataType data;
+}QueueNode;
+typedef struct Queue
 {
-    int size; //已添加的人数
-    int capacity;//最大容量
-    struct PeoInfo* data;//个人信息数组
-    
-};
-//初始化
-void InitContact(struct Contact* cp);
-//打印显示
-void ShowContact(struct Contact* cp);
-//增加
-void ADD(struct Contact* cp);
-//删减
-void Delete(struct Contact* cp);
-void Search(struct Contact* cp);
-void Modify(struct Contact* cp);
-void Sort(struct Contact* cp);
-void check(struct Contact* cp);
-void destroy(struct Contact* cp);
-const int FindByName(struct Contact* cp,char name[MAX_NAME]);
-// *********************************************
-
-
-
-
-
-
-
-
-//***************************************************************
-void mean()
+    struct QueueNode* head;
+    struct QueueNode* tail;
+}Queue;
+void QueueInit(Queue* ps);
+void QueueDestroy(Queue* ps);
+void QueuePush(Queue* ps,QueueDataType x);
+void QueuePop(Queue* ps);
+QueueDataType QueueFront(Queue* pq);
+QueueDataType QueueBuck(Queue* pq);
+int QueueSize(Queue* ps);
+bool QueueEmpty(Queue* ps);
+/***********************************************************/
+void QueueInit(Queue* pq)
 {
-    printf("*************************************\n");
-    printf("***********1.ADD       2.Delete******\n");
-    printf("***********3.Search    4.Modify******\n");
-    printf("***********5.Show      6.Sort********\n");
-    printf("***********0.Exit********************\n");
+    assert(pq);
+    pq->head = NULL;
+    pq->tail = NULL;
 }
-
-int main()
+void QueueDestroy(Queue* pq)
 {
-    
-    int input = 0;
-    //创建通讯录
-    struct Contact con;
-   
-    InitContact(&con);
-    do
+    assert(pq);
+    QueueNode* cur = pq->head;
+    while(cur)
     {
-        mean();
-        printf("请选择>");
-        scanf("%d",&input);
-        switch (input)
-        {
-            case 1:
-                ADD(&con);
-                break;
-            case 2:
-                Delete(&con);
-                break;
-            case 3:
-                Search(&con);
-                break;
-            case 4:
-                Modify(&con);
-                break;
-            case 5:
-                ShowContact(&con);
-                break;
-            case 6:
-                Sort(&con);
-                break;            
-            default:
-                break;
-        }
-        /* code */
-    } while (input);
-    destroy(&con);
-    return 0;
+        QueueNode* tmp = cur->next;
+        free(cur);
+        cur = tmp;
+    }
+    pq->head = NULL;
+    pq->tail = NULL;
 }
-//****************************************************************
-
-
-// ****************************************
-#include <stdio.h>
-// #include "contact.h"
-
-void InitContact(struct Contact* cp)
+void QueuePush(Queue* pq,QueueDataType x)
 {
-    cp->data = (struct PeoInfo*)malloc(sizeof(struct PeoInfo)*3);
-    cp->size = 0;
-    cp->capacity = CAPACITY;
-}
-
-void ShowContact(struct Contact* cp)
-{
-    if(cp->size == 0)
+    assert(pq);
+    QueueNode* newnode = (QueueNode*)malloc(sizeof(QueueNode));
+    newnode->next = NULL;
+    newnode->data = x;
+    if(pq->head == NULL)
     {
-        printf("通讯录为空\n");
+        pq->head = pq->tail = newnode;
     }
     else
     {
-        printf("%-20s\t%-5s\t%-12s\t%-20s\n","名字","性别","电话","地址");
-        for (int i = 0; i < cp->size; i++)
-        {
-            printf("%-20s\t%-5s\t%-12s\t%-20s\n",
-            cp->data[i].name,
-            cp->data[i].Sex,
-            cp->data[i].Tele,
-            cp->data[i].AD);
-            /* code */
-        }
+        pq->tail->next = newnode;
+        pq->tail = newnode;
     }
 }
-
-void ADD(struct Contact* cp)
+void QueuePop(Queue* pq)
 {
-    check(cp);
-    printf("请输入联系人信息>\n");
-    printf("姓名>");
-    scanf("%s",&cp->data[cp->size].name);
-    printf("性别>");
-    scanf("%s",&cp->data[cp->size].Sex);
-    printf("电话>");
-    scanf("%s",&cp->data[cp->size].Tele);
-    printf("地址>");
-    scanf("%s",&cp->data[cp->size].AD);
-    cp->size++;
+    assert(pq);
+    assert(!QueueEmpty(pq));
+
+    QueueNode* tmp = pq->head->next;
+    free(pq->head);
+    pq->head = tmp;
+    if(pq->head == NULL)//防止整个队列已删完，但tail未置空，成为野指针
+        pq->tail = NULL;
+}
+QueueDataType QueueFront(Queue* pq)
+{
+    assert(pq);
+    assert(!QueueEmpty(pq));
+    return pq->head->data;
+}
+QueueDataType QueueBuck(Queue* pq)
+{
+    assert(pq);
+    assert(!QueueEmpty(pq));
+    return pq->tail->data;
+}
+int QueueSize(Queue* pq)
+{
+    int n = 0;
+    QueueNode* cur = pq->head;
+    while(cur)
+    {
+        n++;
+        cur = cur->next;
+    }
+    return n;
+}
+bool QueueEmpty(Queue* pq)
+{
+    if(pq->head == NULL)
+        return true;
+    else
+        return false;
 }
 
-void Delete(struct Contact* cp)
+/********************本题****************************/
+typedef struct 
 {
-    char name[MAX_NAME];
-    if(cp->size == 0)
+    Queue q1;
+    Queue q2;
+} MyStack;
+
+
+MyStack* myStackCreate() 
+{
+    MyStack* obj = (MyStack*)malloc(sizeof(MyStack));
+    QueueInit(&obj->q1);
+    QueueInit(&obj->q2);
+
+    return obj;
+}
+
+void myStackPush(MyStack* obj, int x) 
+{
+    if(QueueEmpty(&obj->q1))
     {
-        printf("通讯录为空\n");
+        QueuePush(&obj->q2,x);
     }
     else
     {
-        printf("请输入要删除联系人的名字>");
-        scanf("%s",&name);
-        int pos = FindByName(cp,name);
-        if(-1 == pos)
-        {
-            printf("找不到该联系人\n");
-        }
-        else
-        {
-            for(int i = pos;i < cp->size-1;i++)
-            {
-                cp->data[i] = cp->data[i+1];
-            }
-            cp->size--;
-            printf("删除成功\n");
-        }
-        
+        QueuePush(&obj->q1,x);
     }
 }
 
-//-1表示找不到，
-const int FindByName(struct Contact* cp,char name[MAX_NAME])
+int myStackPop(MyStack* obj) 
 {
-    for(int i = 0;i <cp->size;i++)
+    Queue* NoneEm = &obj->q1;
+    Queue* Empty = &obj->q2;
+    if(QueueEmpty(&obj->q1))
     {
-        if(0 == strcmp(cp->data[i].name,name))
-        {
-            return i;
-        }
+        NoneEm = &obj->q2;
+        Empty = &obj->q1;
     }
-    return -1;
-}
-
-void Search(struct Contact* cp)
-{
-    char name[MAX_NAME];
-    printf("请输入你要查找人的姓名>");
-    scanf("%s",name);
-    int pos = FindByName(cp,name);
-    if(-1 == pos)
+    while(QueueSize(NoneEm) > 1)
     {
-        printf("通讯录无此人\n");
+        QueuePush(Empty,QueueFront(NoneEm));
+        QueuePop(NoneEm);
     }
-    else
-    { 
-        printf("%-20s\t%-5s\t%-12s\t%-20s\n","名字","性别","电话","地址");
-        printf("%-20s\t%-5s\t%-12s\t%-20s\n",
-                        cp->data[pos].name,
-                        cp->data[pos].Sex,
-                        cp->data[pos].Tele,
-                        cp->data[pos].AD);
-    }
+    int ret = QueueBuck(NoneEm);
+    QueuePop(NoneEm);
+
+    return ret;
 }
 
-void Modify(struct Contact* cp)
+int myStackTop(MyStack* obj) 
 {
-    char name[MAX_NAME];
-    printf("请输入你要修改人的姓名>");
-    scanf("%s",name);
-    int pos = FindByName(cp,name);
-    if(-1 == pos)
+    Queue* NoneEm = &obj->q1;
+    Queue* Empty = &obj->q2;
+    if(QueueEmpty(&obj->q1))
     {
-        printf("通讯录无此人\n");
+        NoneEm = &obj->q2;
+        Empty = &obj->q1;
     }
-    else
-    { 
-        printf("姓名>");
-        scanf("%s",&cp->data[pos].name);
-        printf("性别>");
-        scanf("%s",&cp->data[pos].Sex);
-        printf("电话>");
-        scanf("%s",&cp->data[pos].Tele);
-        printf("地址>");
-        scanf("%s",&cp->data[pos].AD);
-    }
+
+    return QueueBuck(NoneEm);
 }
 
-void Sort(struct Contact* cp)
+bool myStackEmpty(MyStack* obj) 
 {
-    for(int i = 0;i < cp->size-1;i++)
-    {
-        for(int j = i;j < cp->size-1-i;j++)
-        {
-            if( strcmp(cp->data[j].name,cp->data[j+1].name) >0 )
-            {
-                char tmp[MAX_NAME];
-                strcpy(tmp,cp->data[j].name);
-                strcpy(cp->data[j].name , cp->data[j+1].name);
-                strcpy(cp->data[j+1].name , tmp);
-
-                // strcpy(tmp,cp->data[j].name);
-                // strcpy(cp->data[j].name , cp->data[j+1].name);
-                // strcpy(cp->data[j+1].name , tmp);
-                //                 strcpy(tmp,cp->data[j].name);
-                // strcpy(cp->data[j].name , cp->data[j+1].name);
-                // strcpy(cp->data[j+1].name , tmp);
-                //                 strcpy(tmp,cp->data[j].name);
-                // strcpy(cp->data[j].name , cp->data[j+1].name);
-                // strcpy(cp->data[j+1].name , tmp);
-            }
-        }
-    }
+    return QueueEmpty(&obj->q1) && QueueEmpty(&obj->q2);
 }
 
-void check(struct Contact * cp)
+void myStackFree(MyStack* obj) 
 {
-    if(cp->size == cp->capacity)
-    {
-        struct PeoInfo* ptr= (struct PeoInfo*)realloc(cp->data,(cp->capacity+2)*sizeof(struct PeoInfo));
-        if(ptr != NULL)
-        {
-            cp->data = ptr;
-            cp->capacity += 2;
-            printf("开辟成功\n");
-        }
-        else
-        {
-            printf("增容失败\n");
-        }
-    }
-}
-void destroy(struct Contact* cp)
-{
-    free(cp->data);
-    cp->data = NULL;
+    QueueDestroy(&obj->q1);
+    QueueDestroy(&obj->q2);
+    free(obj);
 }
 
-// ****************************************
+/**
+ * Your MyStack struct will be instantiated and called as such:
+ * MyStack* obj = myStackCreate();
+ * myStackPush(obj, x);
+ 
+ * int param_2 = myStackPop(obj);
+ 
+ * int param_3 = myStackTop(obj);
+ 
+ * bool param_4 = myStackEmpty(obj);
+ 
+ * myStackFree(obj);
+*/
+
+作者：小呀小菜鸟
+链接：https://leetcode.cn/problems/implement-stack-using-queues/solutions/1893962/by-zhang_zhenxi-a8m3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
