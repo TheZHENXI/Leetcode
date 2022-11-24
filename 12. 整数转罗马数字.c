267eccd792fcabde6c565a911c9c6ff54ed564c0
TheZHENXI <111845931+TheZHENXI@users.noreply.github.com>
2022-11-24 22:11:19 +0800

    Update 12. 整数转罗马数字.c

--- 12. 整数转罗马数字.c
+++ 12. 整数转罗马数字.c
@@ -3,8 +3,6 @@
  * @Date: 2022-10-19 22:38:28
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
  * @LastEditTime: 2022-10-19 22:38:32
- * @FilePath: /CCODE/LC/2.c
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 char * intToRoman(int num)
 {
